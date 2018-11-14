fd907b2a3560dc768c4235d2e0a94701519be78d
Xiutao Liu <xiutao.liu@gruden.com>
2018-11-14 10:34:14 +0800

    AT3-1561 separate the CN ID and Publish Date search.

--- examples/nodejs/AngularJS/config.js
+++ examples/nodejs/AngularJS/config.js
@@ -1,2 +1,2 @@
-var findByIdUrl="https://rrqcsg42vk.execute-api.ap-southeast-2.amazonaws.com/poc/ocds/findById/";
-var findByDatesUrl="https://rrqcsg42vk.execute-api.ap-southeast-2.amazonaws.com/poc/ocds/findByDates/";
+var findByIdUrl="https://dev-tenders-ocds-api.tenders.gov.au/dev/ocds/findById/";
+var findByPublishDateUrl="https://dev-tenders-ocds-api.tenders.gov.au/dev/ocds/findByDates/contractPublished/";
--- examples/nodejs/AngularJS/js/apiSearch.js
+++ examples/nodejs/AngularJS/js/apiSearch.js
@@ -1,32 +1,44 @@
 var app = angular.module('webApp', []);
 
 app.controller('searchForm', function($scope, $http) {
-	$scope.search = function(cnId) {
+	$scope.searchById = function(cnId){
 		if(!cnId){cnId = $scope.cnId;}else{$scope.cnId=cnId;}
 		
-		$scope.showResults = false;
-		if(!cnId && !$scope.dateStart && !$scope.dateEnd){
-			$scope.errorMessage = "You must enter CN ID or Publish Date."
+		if(!cnId){
+			$scope.errorMessage = "You must enter CN ID."
 		}
 		else{
-			$scope.errorMessage = ""
-			$scope.loading = true;
-			$http({
-				method: 'GET',
-				url: findByIdUrl + cnId
-			}).then(function successCallback(response) {
-				$scope.showResults = true;
-				$scope.data = response.data;
-				$scope.cnId = cnId;
-			}, function errorCallback(response) {
-				$scope.showResults = true;
-				$scope.data = response.data;
-			}).finally(function() {
-				$scope.loading = false;
-			});
+			$scope.search(findByIdUrl + cnId);
+			$scope.cnId = cnId;
+		}
+	}
+
+	$scope.searchByPublishDate = function(){
+		if(!$scope.dateStart || !$scope.dateEnd){
+			$scope.errorMessage = "You must enter Publish Date."
+		}
+		else{
+			$scope.search(findByPublishDateUrl + $scope.dateStart + "/" + $scope.dateEnd);
 		}
 		
-		
+	}
+	$scope.search = function(url) {
+		$scope.showResults = false;
+		$scope.errorMessage = ""
+		$scope.loading = true;
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function successCallback(response) {
+			$scope.showResults = true;
+			$scope.data = response.data;
+			console.log(response.data);
+		}, function errorCallback(response) {
+			$scope.showResults = true;
+			$scope.data = response.data;
+		}).finally(function() {
+			$scope.loading = false;
+		});
 	};
 });
 
